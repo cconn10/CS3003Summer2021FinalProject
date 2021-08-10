aaea7daae5516218c8e72a608819ee68d07e044a
VivecIndoril <71391471+VivecIndoril@users.noreply.github.com>
2021-08-09 23:25:18 -0400

    Add files via upload

---
+++ CS3003Summer2021FInalProject/test.cpp
@@ -0,0 +1,16 @@
+// New Features: double data type,
+//               for loop control structure,
+//               binary power operator
+
+int main() {
+  double d;
+  int modSum;
+  d = 1.0;
+  multSum = 0;
+
+  for(int i=5; i<5; i++) {
+    multSum = multSum + i*5;
+  }
+
+  dCubed = d**3;
+}
